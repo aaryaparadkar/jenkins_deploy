c6dfe36e23459b1c98662628147d9d613749c6d3
Aryan Singh <aryansingh6011@gmail.com>
2023-10-02 23:21:43 +0530

    Build

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,11 @@
-import React, { Component } from "react";
+import React, { } from "react";
 
 //stateless Fucntional componet
 
 const NavBar = (props) => {
   return (
     <nav className="navbar navbar-light bg-light">
-      <a className="navbar-brand" href="#">
+      <a className="navbar-brand" href="/">
         Navbar <span>{props.totalCounters}</span>
       </a>
     </nav>
